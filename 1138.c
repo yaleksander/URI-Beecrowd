e295422746a7f90b1fddb62ab962dfe23ac9b613
Russo <yaleksander42@gmail.com>
2025-10-24 00:44:13 -0300

    solution to 1138

---
+++ 1138.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <math.h>
+
+int ord(int n)
+{
+	int e = 0;
+	while (n > 0)
+	{
+		n /= 10;
+		e++;
+	}
+	return e;
+}
+
+void count(int num, int d[10], int i)
+{
+	while (num > 0)
+	{
+		d[num % 10] += i;
+		num /= 10;
+	}
+}
+
+int main()
+{
+	int a, b, d[10], e, k, p, n;
+	scanf("%i%i", &a, &b);
+	if (b == 0)
+		return 0;
+	do
+	{
+/*
+		for (int i = 0; i < 10; i++)
+			d[i] = 0;
+		p = a;
+		while (p <= b)
+			count(p++, d, 1);
+		printf("%i", d[0]);
+		for (int i = 1; i < 10; i++)
+			printf(" %i", d[i]);
+		printf("\n");
+*/
+		for (int i = 0; i < 10; i++)
+			d[i] = 0;
+		while (a <= b)
+		{
+			p = 10000000;
+			while (p > 1 && b - a < p)
+				p /= 10;
+			while (p > 1 && (a / p) % 10 == 9)
+				p /= 10;
+			while (p > 1 && ord(p) > ord(a) + 1)
+				p /= 10;
+			e = ord(p) - 1;
+			k = (a / p) % 10;
+			if (p > 1 && k < 9)
+			{
+				count(a / (p * 10), d, p);
+				for (int i = 0; i < 10; i++)
+					d[i] += (p / 10) * e;
+				d[k + 1] += a % p;
+				if (a >= p)
+					d[k] += p - a % p;
+				a += p;
+				continue;
+			}
+			count(a++, d, 1);
+		}
+		printf("%i", d[0]);
+		for (int i = 1; i < 10; i++)
+			printf(" %i", d[i]);
+		printf("\n");
+		scanf("%i%i", &a, &b);
+	}
+	while (b != 0);
+	return 0;
+}
